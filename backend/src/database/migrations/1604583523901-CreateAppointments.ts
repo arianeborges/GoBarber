e11b7866243dacd4ebdf5731e44a21444fbede82
ariane <ariianeboorges@gmail.com>
2020-11-05 15:11:54 +0000

    Campos criação e edição de registros na tabela

--- backend/src/database/migrations/1604583523901-CreateAppointments.ts
+++ backend/src/database/migrations/1604583523901-CreateAppointments.ts
@@ -22,6 +22,16 @@ export default class CreateAppointments1604583523901
             name: 'date',
             type: 'timestamp with time zone',
           },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
         ],
       }),
     );
