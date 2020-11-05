6a6480ce775fa32eb53ccfb224b20da59bb33bae
ariane <ariianeboorges@gmail.com>
2020-11-05 15:14:23 +0000

    Criação da tabela usuários no BD

---
+++ backend/src/models/User.ts
@@ -0,0 +1,32 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+// represents how the data is composed
+// which fields are
+@Entity('users')
+class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default User;
