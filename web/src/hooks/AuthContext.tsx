8620561ec8c129c73298e9a065591d82a48ff325
Ariane Borges <ariianeboorges@gmail.com>
2020-11-13 11:44:17 +0000

    Logout da aplicação

--- web/src/hooks/AuthContext.tsx
+++ web/src/hooks/AuthContext.tsx
@@ -17,6 +17,7 @@ interface AuthContextData {
   // eslint-disable-next-line @typescript-eslint/ban-types
   user: object;
   signIn(credentials: SignInCredentials): Promise<void>;
+  signOut(): void;
 }
 
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
@@ -47,8 +48,15 @@ export const AuthProvider: React.FC = ({ children }) => {
     setData({ token, user });
   }, []);
 
+  const signOut = useCallback(() => {
+    localStorage.removeItem('@GoBarber: token');
+    localStorage.removeItem('@GoBarber: user');
+
+    setData({} as AuthState);
+  }, []);
+
   return (
-    <AuthContext.Provider value={{ user: data.user, signIn }}>
+    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
       {children}
     </AuthContext.Provider>
   );
