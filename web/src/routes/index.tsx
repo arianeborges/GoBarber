2da91f1a48d19ce6cd5808fa50e4290dea951b27
Ariane Borges <ariianeboorges@gmail.com>
2020-11-13 16:41:04 +0000

    Rotas autenticadas

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import { Switch } from 'react-router-dom';
+import Dashboard from '../pages/Dashboard';
 
-import { Route, Switch } from 'react-router-dom';
+import Route from './Route';
 
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
@@ -9,6 +11,7 @@ const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={SignIn} />
     <Route path="/signup" component={SignUp} />
+    <Route path="/dashboard" component={Dashboard} isPrivate />
   </Switch>
 );
 
