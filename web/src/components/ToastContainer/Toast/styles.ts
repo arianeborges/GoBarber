fe841198035ba7bbc12266efd34d5ee5581189a2
Ariane Borges <ariianeboorges@gmail.com>
2020-11-13 16:05:20 +0000

    Animação dos Toasts

--- web/src/components/ToastContainer/Toast/styles.ts
+++ web/src/components/ToastContainer/Toast/styles.ts
@@ -1,5 +1,7 @@
 import styled, { css } from 'styled-components';
 
+import { animated } from 'react-spring';
+
 interface ContainerProps {
   type?: 'success' | 'error' | 'info';
   hasDescription: boolean;
@@ -22,7 +24,7 @@ const toastTypeVariations = {
   `,
 };
 
-export const Container = styled.div<ContainerProps>`
+export const Container = styled(animated.div)<ContainerProps>`
   width: 360px;
 
   position: relative;
