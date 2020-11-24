fa837774a435552467d9cf838cf13b4d74bd6ec4
Ariane Borges <ariianeboorges@gmail.com>
2020-11-24 18:23:27 +0000

    Testes de autenticação completo

--- backend/src/modules/users/services/AuthenticateUserService.spec.ts
+++ backend/src/modules/users/services/AuthenticateUserService.spec.ts
@@ -28,5 +28,35 @@ describe('AuthenticateUser', () => {
     expect(response.user).toEqual(user);
   });
 
+  it('should be able to authenticate with non existing user', async () => {
+    const fakeAppointmentsRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const authenticateUser = new AuthenticateUserService(fakeAppointmentsRepository, fakeHashProvider);
+
+    expect(authenticateUser.execute({
+      email: 'mary@gmail.com',
+      password: '123456'
+    })).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should be able to authenticate with wrong password', async () => {
+    const fakeAppointmentsRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const createUser = new CreateUserService(fakeAppointmentsRepository, fakeHashProvider);
+    const authenticateUser = new AuthenticateUserService(fakeAppointmentsRepository, fakeHashProvider);
+
+    await createUser.execute({
+      name: 'Mary Loe',
+      email: 'mary@example.com',
+      password: '123456'
+    });
+
+    expect(authenticateUser.execute({
+      email: 'mary@example.com',
+      password: '1234567'
+    })).rejects.toBeInstanceOf(AppError);
+  });
 
 });
