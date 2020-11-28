4b2efffb08ee87a43298607ca4a45b0f512d9b16
Ariane Borges <ariianeboorges@gmail.com>
2020-11-28 15:41:06 +0000

    Agenda do prestador de serviço

---
+++ backend/src/modules/appointments/services/ListProviderAppointmentsService.spec.ts
@@ -0,0 +1,39 @@
+import 'reflect-metadata';
+// import AppError from '@shared/errors/AppError';
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import ListProviderAppointmentsService from './ListProviderAppointmentsService';
+
+let listProviderAppointments: ListProviderAppointmentsService;
+let fakeAppointmentsRepository: FakeAppointmentsRepository;
+
+describe('ListProviderAppointments', () => {
+  beforeEach(() => {
+    fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    listProviderAppointments = new ListProviderAppointmentsService(
+      fakeAppointmentsRepository,
+    );
+  });
+
+  it('should be able to list appointments on a  specific day', async () => {
+    const appointment1 = await fakeAppointmentsRepository.create({
+      user_id: '1',
+      provider_id: '123',
+      date: new Date(2020, 11, 29, 8, 0, 0),
+    });
+
+    const appointment2 = await fakeAppointmentsRepository.create({
+      user_id: '1',
+      provider_id: '123',
+      date: new Date(2020, 11, 29, 9, 0, 0),
+    });
+
+    const appointments = await listProviderAppointments.execute({
+      provider_id: '123',
+      day: 29,
+      year: 2020,
+      month: 12,
+    });
+
+    expect(appointments).toEqual([appointment1, appointment2]);
+  });
+});
